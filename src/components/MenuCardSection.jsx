19776c018f038e754085498d15261767e3351f32
obidyhasan <obidyhasan@gmail.com>
2025-01-01 12:47:00 +0600

    include loading functionalitly

--- src/components/MenuCardSection.jsx
+++ src/components/MenuCardSection.jsx
@@ -3,16 +3,22 @@ import MenuCard from "./MenuCard";
 import PropTypes from "prop-types";
 
 const MenuCardSection = ({ categoryItem }) => {
-  const [menus] = useMenu();
+  const [menus, loading] = useMenu();
   const item = menus.filter((menu) => menu.category === categoryItem);
 
   return (
     <div>
-      <div className="my-10 grid grid-cols-1 md:grid-cols-2 gap-5">
-        {item.map((menu) => (
-          <MenuCard key={menu._id} menuItem={menu}></MenuCard>
-        ))}
-      </div>
+      {loading ? (
+        <div className="text-center my-20">
+          <span className="loading loading-spinner loading-lg"></span>
+        </div>
+      ) : (
+        <div className="my-10 grid grid-cols-1 md:grid-cols-2 gap-5">
+          {item.map((menu) => (
+            <MenuCard key={menu._id} menuItem={menu}></MenuCard>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
--- src/hooks/useMenu.jsx
+++ src/hooks/useMenu.jsx
@@ -2,13 +2,17 @@ import { useEffect, useState } from "react";
 
 const useMenu = () => {
   const [menus, setMenus] = useState([]);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     fetch("http://localhost:5000/api/menus")
       .then((res) => res.json())
-      .then((data) => setMenus(data));
+      .then((data) => {
+        setMenus(data);
+        setLoading(false);
+      });
   }, []);
 
-  return [menus];
+  return [menus, loading];
 };
 
 export default useMenu;
