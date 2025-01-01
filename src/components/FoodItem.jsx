8da4a4603f688f460c5ff00728552cec771fed02
obidyhasan <obidyhasan@gmail.com>
2025-01-02 01:38:25 +0600

    include food category

--- src/components/FoodItem.jsx
+++ src/components/FoodItem.jsx
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 
 const FoodItem = ({ food }) => {
-  const { name, image, price, recipe } = food;
+  const { name, image, price, recipe, category } = food;
 
   return (
     <div>
@@ -14,6 +14,7 @@ const FoodItem = ({ food }) => {
         </p>
       </div>
       <div className="bg-gray-100 text-center space-y-3 p-4">
+        <div className="badge">{category}</div>
         <h2 className="font-semibold text-lg">{name}</h2>
         <p className="line-clamp-2">{recipe}</p>
         <button className="border-b-4 btn btn-outline border-x-0 border-t-0 shadow-lg text-yellow-600 border-yellow-600">
