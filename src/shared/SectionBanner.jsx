ccc5fa01a8dc064df0c9c1014713ca854b277a00
obidyhasan <obidyhasan@gmail.com>
2025-01-02 01:53:00 +0600

    include parallax effect on background

--- src/shared/SectionBanner.jsx
+++ src/shared/SectionBanner.jsx
@@ -1,14 +1,10 @@
 import PropTypes from "prop-types";
+import { Parallax } from "react-parallax";
 
 const SectionBanner = ({ title, description, img }) => {
   return (
     <div>
-      <div
-        className="hero min-h-[400px]"
-        style={{
-          backgroundImage: `url(${img})`,
-        }}
-      >
+      {/* <div className="hero min-h-[400px]">
         <div className="hero-overlay bg-opacity-60 "></div>
         <div className="hero-content text-center text-white">
           <div className="max-w-3xl">
@@ -16,7 +12,23 @@ const SectionBanner = ({ title, description, img }) => {
             <p className="mb-5">{description}</p>
           </div>
         </div>
-      </div>
+      </div> */}
+
+      <Parallax
+        className="hero"
+        blur={10}
+        bgImage={img}
+        bgImageAlt="the cat"
+        strength={300}
+      >
+        <div className="hero-overlay bg-opacity-60 "></div>
+        <div className="hero-content text-center text-white py-32">
+          <div className="max-w-3xl ">
+            <h1 className="mb-5 text-3xl font-bold uppercase">{title}</h1>
+            <p className="mb-5">{description}</p>
+          </div>
+        </div>
+      </Parallax>
     </div>
   );
 };
