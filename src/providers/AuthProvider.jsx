1f7fe04b52d4c4da2e84fe58a8a60008ae976ce4
obidyhasan <obidyhasan@gmail.com>
2025-01-02 15:07:03 +0600

    include Auth Provider Functionality

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,8 +1,62 @@
 import PropTypes from "prop-types";
 import AuthContext from "../contexts/AuthContext";
+import { useEffect, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import { auth } from "../firebase/firebase.config";
 
 const AuthProvider = ({ children }) => {
-  const authInfo = {};
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const authInfo = {
+    user,
+    loading,
+    setLoading,
+    userLoginInFirebase,
+    userRegisterInFirebase,
+    userLogoutInFirebase,
+    userProfileUpdateInFirebase,
+  };
+
+  const userLoginInFirebase = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const userRegisterInFirebase = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const userLogoutInFirebase = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  const userProfileUpdateInFirebase = (name, photo) => {
+    setLoading(true);
+    return updateProfile(auth.currentUser, {
+      displayName: name,
+      photoURL: photo,
+    });
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
 
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
