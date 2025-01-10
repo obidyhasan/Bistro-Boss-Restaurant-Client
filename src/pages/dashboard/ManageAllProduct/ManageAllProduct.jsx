d7c26672a41f5723ecaab63c263ad19129cec841
obidyhasan <obidyhasan@gmail.com>
2025-01-10 11:06:29 +0600

    include Product Delete Functionality

--- src/pages/dashboard/ManageAllProduct/ManageAllProduct.jsx
+++ src/pages/dashboard/ManageAllProduct/ManageAllProduct.jsx
@@ -3,11 +3,14 @@ import Heading from "../../../components/Heading";
 import { FaEdit } from "react-icons/fa";
 import useMenu from "../../../hooks/useMenu";
 import Swal from "sweetalert2";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { showSweetAlert } from "../../../utility/showSweetAlert";
+import { Link } from "react-router-dom";
 
 const ManageAllProduct = () => {
   const [menus, , refetch] = useMenu();
+  const axiosSecure = useAxiosSecure();
 
-  console.log(menus);
   function handelItemDelete(item) {
     Swal.fire({
       title: "Are you sure?",
@@ -17,13 +20,13 @@ const ManageAllProduct = () => {
       confirmButtonColor: "#3085d6",
       cancelButtonColor: "#d33",
       confirmButtonText: "Yes, delete it!",
-    }).then((result) => {
+    }).then(async (result) => {
       if (result.isConfirmed) {
-        Swal.fire({
-          title: "Deleted!",
-          text: "Your file has been deleted.",
-          icon: "success",
-        });
+        const res = await axiosSecure.delete(`/api/menus/${item._id}`);
+        if (res.data.deletedCount > 0) {
+          refetch();
+          showSweetAlert("success", "Deleted Item Successfully");
+        }
       }
     });
   }
@@ -65,16 +68,15 @@ const ManageAllProduct = () => {
                   <td className="font-semibold">{menu.name}</td>
                   <td>{menu.price}</td>
                   <th>
-                    <button
-                      onClick={() => handelItemDelete(menu)}
-                      className="btn btn-ghost text-2xl"
-                    >
-                      <FaEdit></FaEdit>
-                    </button>
+                    <Link to={`/dashboard/admin/update-product/${menu._id}`}>
+                      <button className="btn btn-ghost text-2xl">
+                        <FaEdit></FaEdit>
+                      </button>
+                    </Link>
                   </th>
                   <th>
                     <button
-                      onClick={() => handelItemDelete()}
+                      onClick={() => handelItemDelete(menu)}
                       className="btn btn-ghost text-orange-600 text-2xl"
                     >
                       <MdDelete></MdDelete>
