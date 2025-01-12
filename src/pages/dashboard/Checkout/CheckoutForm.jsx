87cf180d0accf4bebdfc934f5a3c936803de0023
obidyhasan <obidyhasan@gmail.com>
2025-01-12 22:52:14 +0600

    include CheckoutForm Page

--- src/pages/dashboard/Checkout/CheckoutForm.jsx
+++ src/pages/dashboard/Checkout/CheckoutForm.jsx
@@ -1,13 +1,86 @@
-import { PaymentElement } from "@stripe/react-stripe-js";
+import {
+  CardElement,
+  PaymentElement,
+  useElements,
+  useStripe,
+} from "@stripe/react-stripe-js";
+import { useEffect, useState } from "react";
+import useCart from "../../../hooks/useCart";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import useAuth from "../../../hooks/useAuth";
 
 const CheckoutForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [cart] = useCart();
+  const [clientSecret, setClientSecret] = useState();
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+
+  const totalPrice = cart.reduce((total, item) => total + item.price, 0);
+
+  useEffect(() => {
+    if (totalPrice > 0) {
+      axiosSecure
+        .post("/api/create-payment-intent", { price: totalPrice })
+        .then((res) => {
+          console.log(res.data);
+          setClientSecret(res.data.clientSecret);
+        });
+    }
+  });
+
+  const handelSubmit = async (event) => {
+    event.preventDefault();
+
+    if (!stripe || !elements) {
+      return;
+    }
+
+    const card = elements.getElement(CardElement);
+
+    if (card == null) return;
+
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card,
+    });
+
+    if (error) {
+      console.log("payment error", error);
+      setErrorMessage(error.message);
+    } else {
+      console.log("payment method", paymentMethod);
+      setErrorMessage("");
+    }
+
+    // Confirm Payment
+    const { paymentIntent, error: confirmError } =
+      await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          billing_details: {
+            name: user.displayName || "anonymous",
+            email: user.email || "anonymous",
+          },
+        },
+      });
+
+    if (confirmError) {
+      console.log("confirm error", confirmError);
+    } else {
+      console.log("payment intent is success", paymentIntent);
+    }
+  };
+
   return (
     <div>
       <h1>Checkout</h1>
-      <form>
+      <form onSubmit={handelSubmit}>
         <PaymentElement></PaymentElement>
         <button className="btn">Submit</button>
       </form>
+      <p className="text-red-500">{errorMessage}</p>
     </div>
   );
 };
