8722d50b5ab133f3d5d48bbd642c3adb0208e8de
obidyhasan <obidyhasan@gmail.com>
2025-01-13 00:08:52 +0600

    include Payment History Page

--- src/pages/dashboard/PaymentHistory/PaymentHistory.jsx
+++ src/pages/dashboard/PaymentHistory/PaymentHistory.jsx
@@ -1,7 +1,51 @@
+import Heading from "../../../components/Heading";
+import { useQuery } from "@tanstack/react-query";
+import useAuth from "../../../hooks/useAuth";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+
 const PaymentHistory = () => {
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+
+  const { data: payments = [] } = useQuery({
+    queryKey: ["payments", user?.email],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/api/payments/${user?.email}`);
+      return res.data;
+    },
+  });
+
+  console.log(payments);
+
   return (
     <div>
-      <h1>Payment History Page</h1>
+      <Heading heading={"Payment History"} subHeading={""}></Heading>
+
+      <div className="mt-10">
+        <div className="overflow-x-auto">
+          <table className="table">
+            {/* head */}
+            <thead>
+              <tr>
+                <th></th>
+                <th>Transaction Id</th>
+                <th>Price</th>
+                <th>Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              {payments.map((payment, idx) => (
+                <tr key={payment._id}>
+                  <th>{idx + 1}</th>
+                  <td>{payment.transactionId}</td>
+                  <td>{payment.price}</td>
+                  <td>{payment.status}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 };
