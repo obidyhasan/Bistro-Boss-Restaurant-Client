0bc51e9bd6b4043687f7d58a9f68e810a91b3c38
obidyhasan <obidyhasan@gmail.com>
2025-01-21 18:43:29 +0600

    include Bar Chart on admin home

--- src/pages/dashboard/AdminHome/AdminHome.jsx
+++ src/pages/dashboard/AdminHome/AdminHome.jsx
@@ -2,11 +2,15 @@ import { useQuery } from "@tanstack/react-query";
 import useAuth from "../../../hooks/useAuth";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid } from "recharts";
+
+const colors = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "red", "pink"];
+
 const AdminHome = () => {
   const { user } = useAuth();
   const axiosSecure = useAxiosSecure();
 
-  const { data } = useQuery({
+  const { data: stats = [] } = useQuery({
     queryKey: ["admin-stats"],
     queryFn: async () => {
       const res = await axiosSecure.get("/api/admin-stats");
@@ -14,7 +18,31 @@ const AdminHome = () => {
     },
   });
 
-  console.log(data);
+  const { data: chartInfo = [] } = useQuery({
+    queryKey: ["chart-info"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/api/admin-chart");
+      return res.data;
+    },
+  });
+
+  const getPath = (x, y, width, height) => {
+    return `M${x},${y + height}C${x + width / 3},${y + height} ${
+      x + width / 2
+    },${y + height / 3}
+    ${x + width / 2}, ${y}
+    C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${
+      x + width
+    }, ${y + height}
+    Z`;
+  };
+
+  const TriangleBar = (props) => {
+    // eslint-disable-next-line react/prop-types
+    const { fill, x, y, width, height } = props;
+
+    return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+  };
 
   return (
     <div>
@@ -26,23 +54,54 @@ const AdminHome = () => {
         <div className="stats shadow flex lg:flex-nowrap flex-wrap">
           <div className="stat place-items-center">
             <div className="stat-title">Total Revenue</div>
-            <div className="stat-value">$ {data?.revenue}</div>
+            <div className="stat-value">$ {stats?.revenue}</div>
           </div>
 
           <div className="stat place-items-center">
             <div className="stat-title">Customer</div>
-            <div className="stat-value text-secondary">{data?.users}</div>
+            <div className="stat-value text-secondary">{stats?.users}</div>
           </div>
 
           <div className="stat place-items-center">
             <div className="stat-title">Total Product</div>
-            <div className="stat-value">{data?.products}</div>
+            <div className="stat-value">{stats?.products}</div>
           </div>
 
           <div className="stat place-items-center">
             <div className="stat-title">Total Order</div>
-            <div className="stat-value">{data?.orders}</div>
+            <div className="stat-value">{stats?.orders}</div>
           </div>
+        </div>
+      </div>
+
+      <div className="mt-10 grid grid-cols-1 xl:grid-cols-2 gap-2">
+        <div className="w-full">
+          <BarChart
+            className="w-full"
+            width={500}
+            height={300}
+            data={chartInfo}
+            margin={{
+              top: 20,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="category" />
+            <YAxis />
+            <Bar
+              dataKey="quantity"
+              fill="#8884d8"
+              shape={<TriangleBar />}
+              label={{ position: "top" }}
+            >
+              {chartInfo.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+              ))}
+            </Bar>
+          </BarChart>
         </div>
       </div>
     </div>
